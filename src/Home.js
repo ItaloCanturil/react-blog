4ef17edd8db316e1903c30ee82f23e9ad80b8762
Italo Canturil <italo.cantur@gmail.com>
2022-03-13 22:16:27 -0300

    feat: Introduce useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [name, setName] = useState('Italo');
+
+  const handleClick = () => {
+    setName('Cabeça')
+  }
+
   return ( 
     <div className="home">
       <h2>Homepage</h2>
+      <p>{ name }</p>
+      <button onClick={handleClick}>Click me</button>
     </div>
    );
 }
