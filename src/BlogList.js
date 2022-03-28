40d9d0f0234d3059e2f9930f07fba4fed017675e
Italo Canturil <italo.cantur@gmail.com>
2022-03-27 23:13:07 -0300

    feat: Pass function how props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,12 @@
-const BlogList = (props) => {
-  const blogs = props.blogs
+const BlogList = ({blogs, handleDelete}) => {
+
   return (
     <div className="blog-list">
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{ blog.title }</h2>
-          <p>{ blog.author }</p>
+          <p>Written by { blog.author }</p>
+          <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
         </div>
       ))}
     </div>
